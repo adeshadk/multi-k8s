d65c26964e66b05bff66e2efa5160ec9f35bad57
adeshadk <adeshak@hotmail.com>
2019-04-15 19:23:07 -0700

    fixed error in deploy.sh again for docker images versions

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t adeshadk/mult-client:latest -t adeshadk/mult-client:$SHA -f ./client/Dockerfile ./client
-docker build -t adeshadk/multi-server -t adeshadk/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t adeshadk/multi-worker -t adeshadk/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t adeshadk/multi-client:latest -t adeshadk/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t adeshadk/multi-server:latest -t adeshadk/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t adeshadk/multi-worker:latest -t adeshadk/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 docker push adeshadk/multi-client:latest
 docker push adeshadk/multi-server:latest
 docker push adeshadk/multi-worker:latest
